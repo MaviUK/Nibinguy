52c276e59c1c81dcbd8f6e0c701be50c8c585338
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-07-22 13:04:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from "react"
 import { Routes, Route } from "react-router-dom"
 import NiBinGuyLandingPage from "./LandingPage"
 import StandeeClaim from "./standee/StandeeClaim"
+import StandeeSpottedClaim from "./standee/StandeeSpottedClaim"
+import StandeePreClaim from "./standee/StandeePreClaim"
 import LatestStandeeRedirect from "./pages/standee/latest"
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<NiBinGuyLandingPage />} />
-      <Route path="/standee/:slug" element={<StandeeClaim />} />
-      <Route path="/standee/latest" element={<LatestStandeeRedirect />} /> {/* ✅ Add this line */}
+      <Route path="/standee/:slug" element={<StandeePreClaim />} />   {/* Pre-claim choice */}
+      <Route path="/standee/:slug/claim" element={<StandeeClaim />} />   {/* I live here */}
+      <Route path="/standee/:slug/spotted" element={<StandeeSpottedClaim />} />  {/* I spotted */}
+      <Route path="/standee/latest" element={<LatestStandeeRedirect />} />
     </Routes>
   )
 }
