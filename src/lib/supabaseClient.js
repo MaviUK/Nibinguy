cfb528041a4a37e86ab50eb1efc03b2fe4047de6
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-08-02 22:23:27 +0100

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,12 @@
-// src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://cvduwxumfnasfpbfpbwk.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2ZHV3eHVtZm5hc2ZwYmZwYndrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNDQ0MjEsImV4cCI6MjA2NzgyMDQyMX0.IIDcHoXIGELwySwb_0aiNCph-oXmElQXQ6p02mSkXtQ';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  }
+});
