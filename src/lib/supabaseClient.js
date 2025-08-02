c3273288642e7e0cb070fcf5bbe90e01b26cab30
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-08-02 22:57:06 +0100

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cvduwxumfnasfpbfpbwk.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2ZHV3eHVtZm5hc2ZwYmZwYndrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNDQ0MjEsImV4cCI6MjA2NzgyMDQyMX0.IIDcHoXIGELwySwb_0aiNCph-oXmElQXQ6p02mSkXtQ'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
-    storage: localStorage // ✅ ensures session is remembered
+    storage: localStorage
   }
 })
